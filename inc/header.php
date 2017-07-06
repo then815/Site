2b38b0d77cdbfc7c7e19cf596fe14e938addfeb4
Tommy Henry <then815@yahoo.com>
2017-07-06 18:32:54 -0500

    Setup
    Setup file structure, sliced psd to png, measured and logged dimensions

---
+++ inc/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../styles/styles.css">    
+<title><?php echo $title;?></title>
+</head>
+<body>
+    <div class="wrapper">
+    <header>
+        <img src="../img/logo.png" alt="open charity logo">
+        <nav>
+        <ul>
+            <li>About Open Charity</li>
+            <li>The Blog</li>
+            <li class="cta">Join Us</li>
+        </ul>
+        </nav>
+    </header>
