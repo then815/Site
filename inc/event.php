9c91721c562f2bed874d80cf88c5b9da675437cd
Tommy Henry <then815@yahoo.com>
2017-07-07 19:20:25 -0500

    Event section
    Created and styled event section

--- inc/event.php
+++ inc/event.php
@@ -1,6 +1,9 @@
 <div class="event">
     <p class="nextEvent">
-        <span class="next">Next Event:</span> June 23rd 2016 18:30 - 21:00
+        <span class="next">Next Event:</span><span class="date"> June 23rd 2016 18:30 - 21:00</span>
     <br/>
-    <span clas="eventLocation">Cancer Research UK, Angel Bilding, 407 St John Street, London EC1V 4AD</span></p>
+    Cancer Research UK, Angel Bilding, 407 St John Street, London EC1V 4AD</p>
+    <span class="button">
+        Register
+    </span>
 </div>
